2afa7cedb12197c8f12fc38f37ed37eae6471402
Hugo <villalobosvillanuevah@uabc.edu.mx>
2023-10-04 15:54:00 -0700

    Este es el inciso 5 de la tarea de Big Data

--- 3_OCT.scala
+++ 3_OCT.scala
@@ -1,3 +1,8 @@
 //4. Crea un arreglo de numero en rango del 1-1000 en pasos de 5 en 5
 val arr = Array.range(1,1000,5)
 
+//5. Cuales son los elementos únicos de la lista Lista(1,3,3,4,6,7,3,7) utilice conversión a conjuntos
+
+val Lista = List (1,3,3,4,6,7,3,7)
+val unicosLista = Lista.distinct
+
