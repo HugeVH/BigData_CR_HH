340339abc56635c6b40185095cd85e919c53cee5
CarlosRAC <reyneiro.acosta@uabc.edu.mx>
2023-10-10 19:40:43 -0700

    se agrego print para pantalla

--- fibonacci.scala
+++ fibonacci.scala
@@ -3,4 +3,6 @@
 
 //## Algoritmo 1
 
-var i = 7 // variable declarada, para calcular la posicion 7 de la serie de fibonacci
+var i = 7 // variable declarada, para calcular la posicion 7 de la serie de fibonacci 
+
+ println("El numero Fibonacci de " + i + " es: " + fib(i))
